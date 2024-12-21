0e766c0210785130db3bb3646ac31c7a6652f450
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2024-12-22 02:42:54 +0900

    now working

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,23 @@
-import Worker from "./worker.ts?worker";
-import MimiumProcessor from "./audioprocessor.js?worker&url";
-import { MimiumProcessorNode, CompileData } from "./workletnode";
-import wasmurl from "mimium-web/mimium_web_bg.wasm?url";
-
+import {
+  setupAudioWorklet,
+  MimiumProcessorNode,
+} from "@mimium/mimium-webaudio";
+import MimiumProcessorUrl from "../node_modules/@mimium/mimium-webaudio/dist/audioprocessor.mjs?url";
 // const processorBlob = new Blob([workletURL], { type: "text/javascript" });
 // const processorUrl = URL.createObjectURL(processorBlob);
 
-const setupAudioWorklet = async (src: string) => {
-  const userMedia = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: false,
-  });
-  let audioNode;
-  const audioContext = new AudioContext({ latencyHint: "interactive" });
-  try {
-    const response = await window.fetch(wasmurl);
-    const wasmBytes = await response.arrayBuffer();
-    try {
-      await audioContext.audioWorklet.addModule(MimiumProcessor);
-    } catch (e) {
-      let err = e as unknown as Error;
-      throw new Error(
-        `Failed to load audio analyzer worklet at url: ${MimiumProcessor}. Further info: ${err.message}`
-      );
-    }
-    audioNode = new MimiumProcessorNode(audioContext, "MimiumProcessor");
-    audioNode.init(wasmBytes, {
-      src: src,
-      samplerate: audioContext.sampleRate,
-      buffersize: 128,//AudioWorklet Always uses 128 for now
-    } as CompileData);
-    audioContext.resume();
-    const microphone = await audioContext.createMediaStreamSource(userMedia);
-
-    microphone.connect(audioNode).connect(audioContext.destination);
-    return { node: audioNode, context: audioContext };
-  } catch (e) {
-    let err = e as unknown as Error;
-    throw new Error(
-      `Failed to load audio analyzer WASM module. Further info: ${err.message}`
-    );
-  }
-};
-
 let g_node: any = undefined;
 let g_context: AudioContext | null = null;
-const main = async (src: string) => {
-  let { node, context } = await setupAudioWorklet(src);
+export const main = async (src: string) => {
+  let { node, context } = await setupAudioWorklet(src, MimiumProcessorUrl);
   g_node = node;
   g_context = context;
 };
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector("#play_button") as HTMLButtonElement;
-  const stopbutton = document.querySelector("#stop_button") as HTMLButtonElement;
+  const stopbutton = document.querySelector(
+    "#stop_button"
+  ) as HTMLButtonElement;
 
   button.addEventListener("click", async () => {
     const textarea = document.querySelector("#src") as HTMLTextAreaElement;
@@ -63,5 +28,5 @@ document.addEventListener("DOMContentLoaded", () => {
     if (g_context) {
       g_context.close();
     }
-  })
+  });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,38 @@
 import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
+  plugins: [
+    topLevelAwait(),
+    dts({ tsconfigPath: "./tsconfig.json" }),
+    checker({ typescript: true }),
+  ],
+  assetsInclude:["node_modules/@mimium/mimium-webaudio/dist/assets/**"],
   build: {
     minify: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "mimium_web_component",
       formats: ["iife"],
+
       fileName: () => "mimium-web-component.js",
     },
     sourcemap: true,
+  },
+  rollupOptions: {
+    external: ["mimium-webaudio"],
+    output: {
+      // 外部化された依存関係のために UMD のビルドで使用する
+      // グローバル変数を提供します
+      globals: {
+        mimium_webaudio: "setupAudioWorklet",
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["@mimium/mimium-webaudio"],
   },
 });
